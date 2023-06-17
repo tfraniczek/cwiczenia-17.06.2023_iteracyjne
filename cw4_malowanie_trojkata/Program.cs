71abbb2c6db6dd84157f11dd2689a8ca6c5e0292
tfraniczek <tymoteusz.franiczek@gmail.com>
2023-06-17 15:52:25 +0200

    1

---
+++ cw4_malowanie_trojkata/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj wysokość trójkąta:");
+        int height = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Trójkąt złożony ze znaków +:");
+
+        int maxWidth = height * 2 - 1;
+        int startOffset = (Console.WindowWidth - maxWidth) / 2;
+
+        for (int row = 1; row <= height; row++)
+        {
+            int numSymbols = row * 2 - 1;
+            int offset = (maxWidth - numSymbols) / 2;
+
+            Console.SetCursorPosition(startOffset + offset, Console.CursorTop);
+            Console.WriteLine(new string('+', numSymbols));
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ cw4_wypisuje_liczby_pierwsze/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbę n:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Pierwsze {0} liczby pierwsze:", n);
+
+        int count = 0;
+        int number = 2;
+
+        while (count < n)
+        {
+            if (IsPrime(number))
+            {
+                Console.WriteLine(number);
+                count++;
+            }
+
+            number++;
+        }
+
+        Console.ReadLine();
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number < 2)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
